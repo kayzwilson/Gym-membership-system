30c6fda072b71ba30cca4f798193fe608d6d35c1
wilson <kayzwilson09@gmail.com>
2026-05-03 21:04:00 +0300

    Made base laravel Project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+   public function run(): void
+{
+    User::create([
+        'name'     => 'Admin',
+        'email'    => 'admin@gym.com',
+        'password' => Hash::make('password'),
+    ]);
+}
+
+    
+}
